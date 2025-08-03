78228822f0475a98a3595ad207c768112ac49c88
ayandambuyane <mbuyaneayanda18@gmail.com>
2025-08-03 06:27:33 +0200

    alias use

--- complex.cpp
+++ complex.cpp
@@ -2,17 +2,19 @@
  #include <complex> // contains the complex class definition
 
 using namespace std; // cout, endl, complex are all part of this namespace
+using cfloat= complex<float>; // using alias for complex<float>
 
  int main()
  {
- auto num1 = complex<float>{2.0, 2.0}; // use auto for type deduction
- auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+ auto num1 = cfloat{2.0, 2.0}; // use auto for type deduction
+ auto num2 = cfloat{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
 
 auto answer = num1 * num2; // type deduced for 'answer' is:complex<float>
  cout << "The answer is: " << answer << endl;
  cout << "Or in a more familiar form: " << answer.real() << " + " << answer.imag() << "j" << endl << endl;
 
- // answer++;  // Uncommenting this line will cause a compilation error
+ //answer++;  
+ // uncommenting the above line will cause a compilation error because complex numbers do not support increment operator
 
 return 0;
  }
