c67f9a1f78a46a45aea5d2906c8729d38de34a2b
ayandambuyane <mbuyaneayanda18@gmail.com>
2025-08-06 23:30:28 +0200

    alias added

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <complex>
 using namespace std;
-
+using cdouble = complex<double>; // using alias for complex<double>
 int main() {
     char  choice;
 do {
-    complex<double> a, b, c;
+    cdouble a, b, c;
     cin >> a >> b >> c;
-   complex<double> discriminant = b * b - 4.0 * a * c;
-    complex<double> root1 = (-b + sqrt(discriminant)) / (2.0 * a);
-    complex<double> root2 = (-b - sqrt(discriminant)) / (2.0 * a);
+   cdouble discriminant = b * b - 4.0 * a * c;
+    cdouble root1 = (-b + sqrt(discriminant)) / (2.0 * a);
+    cdouble root2 = (-b - sqrt(discriminant)) / (2.0 * a);
     cout << "Roots: " << root1 << ", " << root2 << endl;
     cout << "Press 'q' to quit or any other key to continue: ";
     cin >> choice;
 
 } while (choice != 'q');
-//cout << "Do you want to solve another equation? (Press 'q' to quit): "<< endl;
+
     return 0;
 }
